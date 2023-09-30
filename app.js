6f78f5b6ba8fcbbaf34d4b5700e85a0f7e43ca0c
navneet17360 <navneetanand.2021cs@technonjr.org>
2023-09-30 11:24:35 +0530

    created package.json file

--- app.js
+++ app.js
@@ -7,3 +7,15 @@
 //global dependency - use it in any project
 //npm install <packageName> -g - installs the package globally
 //sudo install -g <packageName> (mac)
+
+//package.json - manifest file (store important info about project/package)
+//manual approach (create package.json in the root,create properties etc)
+//npm init (step by step, press enter to skip)
+//npm init -y (everything default)
+
+const _ = require("lodash");
+
+const items = [1, [2, [3, [4]]]];
+
+const newItems = _.flattenDeep(items);   // its a method used for converting a array of arrays into a single array
+console.log(newItems);
