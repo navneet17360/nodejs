619b643704317c55f7399518039a4f8988da6657
navneet17360 <navneetanand.2021cs@technonjr.org>
2023-10-02 16:36:43 +0530

    Event-loop examples

---
+++ extra.js
@@ -0,0 +1,22 @@
+// //npm - global command , comes with node
+// //npm --version or npm --v tells npm current version
+
+// //local dependency - use it only in this particular project
+
+// //npm install <packageName> - installs the package locally
+// //global dependency - use it in any project
+// //npm install <packageName> -g - installs the package globally
+// //sudo install -g <packageName> (mac)
+
+// //package.json - manifest file (store important info about project/package)
+// //manual approach (create package.json in the root,create properties etc)
+// //npm init (step by step, press enter to skip)
+// //npm init -y (everything default)
+
+// const _ = require("lodash");
+
+// const items = [1, [2, [3, [4]]]];
+
+// const newItems = _.flattenDeep(items); // its a method used for converting a array of arrays into a single array
+// console.log(newItems);
+// console.log("hello people");
